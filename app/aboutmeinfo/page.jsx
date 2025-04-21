e15b4a6090d2d57f7288ab65a8a36e2c010576ba
phaewphanwrd <phaewphanwrd@gmail.com>
2025-04-21 16:40:11 +0700

    Data: edited information of aboutmeinfo

--- app/aboutmeinfo/page.jsx
+++ app/aboutmeinfo/page.jsx
@@ -5,20 +5,21 @@ export default function aboutmeinfo() {
   return (
     <div className="m-20 flex flex-col text-lg justify-center place-items-center gap-5 ">
       <div className="flex flex-col gap-10 font-semibold w-250  p-15 max-sm:w-90 max-sm:p-5 max-md:w-150">
-      <div className="fles fles-col gap-10">
-      <div className="text-2xl">Phaewphan Woradanuphan</div>
-       <div className="text-xl font-light">bkk, Thailand</div>
-      </div>
-       
-        <div className="text-4xl justify-end flex gap-5 place-items-center">Work Experience
-            <Image
-                       src="/images/features.png"
-                       alt="research"
-                       width={80}
-                       height={80}
-                       className="pb-5"
-                     />
-                     </div>
+        <div className="fles fles-col gap-10">
+          <div className="text-2xl">Phaewphan Woradanuphan</div>
+          <div className="text-xl font-light">bkk, Thailand</div>
+        </div>
+
+        <div className="text-4xl justify-end flex gap-5 place-items-center">
+          Work Experience
+          <Image
+            src="/images/features.png"
+            alt="research"
+            width={80}
+            height={80}
+            className="pb-5"
+          />
+        </div>
         {/* fullstack */}
         <div className="flex flex-col gap-2">
           Full Stack Developer
@@ -78,78 +79,77 @@ export default function aboutmeinfo() {
         </div>
         {/* praew */}
         <div className="flex flex-col gap-2">
-          freelance
+          : Founder & Operations Manager
           <div className="font-medium">
-            praew.tattoos (Studio) | Jan 2022 – Present
+            Tattoo Studio & Clothing Brand | 2024 – Present
           </div>
           <div className="font-medium">
-            - Designed and developed custom tattoo artwork tailored to each
-            client's vision and personal story.
-            <br />- Communicated effectively with clients to understand their
-            ideas and provide professional advice on design, placement, and
-            aftercare.
-            <br />
-            - Maintained strong client relationships and ensured a welcoming,
-            hygienic, and professional studio experience.
-            <br /> Managed appointments, consultations, and personal branding
-            through social media under the handle @praew.tattoos
+            - Managed end-to-end business operations, client communications, and
+            vendor coordination <br />
+            - Planned and executed projects from concept to delivery under tight
+            timelines <br />
+            - Built digital presence and handled online marketing and customer
+            engagement <br />- Developed workflows and tracked performance for
+            continuous improvement
           </div>
         </div>
       </div>
 
       {/* education */}
-            <div className="flex flex-col gap-5 p-20 place-items-center max-sm:p-5">
-            <div className="text-center  mb-10 ">
-                <div className="text-2xl font-bold flex justify-center place-items-center gap-5">Education and Training
-                <Image
-                  src="/images/layers_11676373.png"
-                  alt="libraries"
-                  width={70}
-                  height={70}
-                  className="pb-5"
-                />                </div>
-                <div className="mr-60 ml-60 mt-2 max-sm:w-70 max-sm:mr-5 max-sm:ml-5 max-md:w-100">"Formal Education & Self-Directed Learning."</div>
-              </div>
-              <div className="flex gap-10 max-sm:flex-col max-sm:gap-5 max-md:flex-col">
-              {/* 2012 */}
-              <div className=" pt-10 shadow-xl p-10 rounded-2xl border-slate-200 w-100 ">
-               
-                <label className="text-xl font-semibold">2012 - 2016</label>
-                <div className="font-semibold"> 
-              Faculty of Architecture and Design | Bachelor Degree of
-              Fine Arts, </div>
-              <div>Ceramics Design (GPA 2.88)</div>
-                <div className="text-sm">King Mongkut's University Of Technology North Bangkok, | Bangkok,
-                Thailand </div>
-              </div>
-              {/* 2022 */}
-              <div className="p-5 pt-10 shadow-xl rounded-2xl border-slate-200 w-100 ">
-               
-                <label className="text-xl font-semibold">2022</label>
-                <div><div className="font-semibold">Upskill UX</div> Become a UX Designer From Scratch #3</div>
-
-              </div>
-              {/* 2024 */}
-              <div className="p-5 pt-10 shadow-xl  rounded-2xl border-slate-200 w-100 ">
-               
-                <label className="text-xl font-semibold">2024</label>
-                <div className="font-medium">
-                <div className="font-semibold">Google</div>Foundations of Project Management
+      <div className="flex flex-col gap-5 p-20 place-items-center max-sm:p-5">
+        <div className="text-center  mb-10 ">
+          <div className="text-2xl font-bold flex justify-center place-items-center gap-5">
+            Education and Training
+            <Image
+              src="/images/layers_11676373.png"
+              alt="libraries"
+              width={70}
+              height={70}
+              className="pb-5"
+            />{" "}
+          </div>
+          <div className="mr-60 ml-60 mt-2 max-sm:w-70 max-sm:mr-5 max-sm:ml-5 max-md:w-100">
+            "Formal Education & Self-Directed Learning."
+          </div>
+        </div>
+        <div className="flex gap-10 max-sm:flex-col max-sm:gap-5 max-md:flex-col">
+          {/* 2012 */}
+          <div className=" pt-10 shadow-xl p-10 rounded-2xl border-slate-200 w-100 ">
+            <label className="text-xl font-semibold">2012 - 2016</label>
+            <div className="font-semibold">
+              Faculty of Architecture and Design | Bachelor Degree of Fine Arts,{" "}
+            </div>
+            <div>Ceramics Design (GPA 2.88)</div>
+            <div className="text-sm">
+              King Mongkut's University Of Technology North Bangkok, | Bangkok,
+              Thailand{" "}
+            </div>
+          </div>
+          {/* 2022 */}
+          <div className="p-5 pt-10 shadow-xl rounded-2xl border-slate-200 w-100 ">
+            <label className="text-xl font-semibold">2022</label>
+            <div>
+              <div className="font-semibold">Upskill UX</div> Become a UX
+              Designer From Scratch #3
+            </div>
+          </div>
+          {/* 2024 */}
+          <div className="p-5 pt-10 shadow-xl  rounded-2xl border-slate-200 w-100 ">
+            <label className="text-xl font-semibold">2024</label>
+            <div className="font-medium">
+              <div className="font-semibold">Software Park </div>CodeCamp
+              Thailand #19 Bangkok, Thailand <br /> Full Stack Web Development
+              Course
             </div>
             <div className="font-medium">
-             <div className="font-semibold">Software Park </div>CodeCamp Thailand #19 Bangkok, Thailand <br/> Full Stack
-              Web Development Course
+              <div className="font-semibold">Google</div>Foundations of Project
+              Management
             </div>
-              </div>
-             
-              </div>
-    
-        
-            </div>
-
+          </div>
+        </div>
+      </div>
 
       <div className="flex gap-5">
-
         {/* education */}
         {/* <div className="flex flex-col gap-5 font-semibold w-150 p-15 rounded-2xl shadow-xl bg-white">
           <div className="text-2xl ">Education and Training</div>
@@ -180,13 +180,13 @@ export default function aboutmeinfo() {
         </div> */}
         {/* technical */}
         <div className="flex flex-col gap-5 font-semibold p-15 w-250  rounded-2xl shadow-xl bg-white max-sm:w-90 max-md:w-120">
-        <Image
-                  src="/images/coding_11998938.png"
-                  alt="languages"
-                  width={70}
-                  height={70}
-                  className="pb-5"
-                />
+          <Image
+            src="/images/coding_11998938.png"
+            alt="languages"
+            width={70}
+            height={70}
+            className="pb-5"
+          />
           <div className="text-2xl ">Technical Skills</div>
           <div className="">
             Software Development:
@@ -210,16 +210,15 @@ export default function aboutmeinfo() {
             <div className="font-medium">
               - Methodologies: Agile, Waterfall <br />
               - Tools: Asana, Google Workspace, Microsoft Office, PowerBI
-              Office, Google Sheets, PowerPoint <br/>
-              - Research Tools: FigJam
+              Office, Google Sheets, PowerPoint <br />- Research Tools: FigJam
             </div>
           </div>
           <div className="flex flex-col gap-2">
-          Development Tools:
+            Development Tools:
             <div className="font-medium">
               - IDEs & Editors: VS Code <br />
-              - Containerization: Docker<br/>
-              - Research Tools: FigJam
+              - Containerization: Docker
+              <br />- Research Tools: FigJam
             </div>
           </div>
           <div className="flex flex-col gap-2">
